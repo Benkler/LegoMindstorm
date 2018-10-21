b2c8a336521f6f92c06bc42c6e41b6901dc1512c
Benkler <benklerniko@t-online.de>
2018-10-22 01:31:59 +0200

    Print String and Motor Pilot test

--- HelloWorld/src/edu/kit/lego/HelloWorld.java
+++ HelloWorld/src/edu/kit/lego/HelloWorld.java
@@ -1,9 +1,27 @@
 package edu.kit.lego;
 
+import lejos.hardware.lcd.LCD;
+import lejos.hardware.lcd.TextLCD;
+import lejos.hardware.motor.Motor;
+import lejos.robotics.navigation.DifferentialPilot;
+import lejos.utility.Delay;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		//System.out.println("Hello World!");
+		
+		LCD.drawString("Hello World", 0, 0);
+		
+		//DifferentialPilot pilot = new DifferentialPilot(wheelDiameter, trackWidth, leftMotor, rightMotor)
+		DifferentialPilot pilot = new DifferentialPilot(1.5f, 10, Motor.A, Motor.B);
+		pilot.travel(15);
+		
+		
+		
+		
+		
+		
 	}
 
 }
